a453fcd5486c232babd790d2c27d6a002d8c4f76
mbrecht <brecht.michael.b@gmail.com>
2021-10-31 10:01:13 -0600

    updates item view

--- src/components/ItemView.js
+++ src/components/ItemView.js
@@ -7,24 +7,22 @@ import capitalizeString from "../util/capitalizeString";
 
 const InfoData = ({ name, value }) => (
   <div className={styles.infoData}>
-    <Typography className={styles.infoDataName}>{name}</Typography>
-    <Typography className={styles.infoDataValue}>{value}</Typography>
+    <Typography className={styles.infoDataName}>
+      {capitalizeString(new String(name))}
+    </Typography>
+    <Typography className={styles.infoDataValue}>
+      {capitalizeString(new String(value))}
+    </Typography>
   </div>
 );
 
-const InfoSection = ({ name, keys = [], data }) => {
+const InfoSection = ({ name, children }) => {
   return (
     <div className={styles.infoSection}>
       <Typography className={styles.infoHeader}>
         {capitalizeString(name)}
       </Typography>
-      {keys.map((key) => (
-        <InfoData
-          name={capitalizeString(key)}
-          value={capitalizeString(new String(data[key]))}
-          key={key}
-        />
-      ))}
+      {children}
     </div>
   );
 };
@@ -47,21 +45,35 @@ const InfoPanel = (props) => {
   const { data } = props;
   return (
     <section {...createProps(props, { className: styles.infoPanel })}>
-      <InfoSection
-        name={data.name}
-        keys={["released", "members", "quest_item"]}
-        data={data}
-      />
+      <InfoSection name={data.name}>
+        <InfoData
+          name="released"
+          value={new Date(data.release_date).toLocaleDateString()}
+        />
+        <img src={`data:image/jpg;base64, ${data.icon}`} />
+        <InfoData name="members" value={data.members} />
+        <InfoData name="quest item" value={data.quest_item} />
+      </InfoSection>
       <InfoSection
         name="properties"
         keys={["tradeable", "equipable", "stackable", "examine"]}
         data={data}
-      />
+      >
+        <InfoData name="tradeable" value={data.members} />
+        <InfoData name="equipable" value={data.equipable} />
+        <InfoData name="stackable" value={data.stackable} />
+        <InfoData name="examine" value={data.examine} />
+      </InfoSection>
       <InfoSection
         name="values"
         keys={["cost", "highalch", "lowalch", "weight"]}
         data={data}
-      />
+      >
+        <InfoData name="cost" value={data.cost.toLocaleString()} />
+        <InfoData name="high alch" value={data.highalch.toLocaleString()} />
+        <InfoData name="low alch" value={data.lowalch.toLocaleString()} />
+        <InfoData name="weight" value={`${data.weight} lbs`} />
+      </InfoSection>
     </section>
   );
 };
